a89929c47d3a4a6456265d5f39576a316dd9a4a4
Mihir Trivedi <mihirplanes@gmail.com>
2014-11-11 11:41:33 -0800

    Turtles werk

--- Turtles_Trivedi.java
+++ Turtles_Trivedi.java
@@ -1,7 +1,15 @@
+/**
+   These imports bring in the graphics programs for the program.
+   They allow for the creation of turtles and lines
+*/
 import acm.graphics.GTurtle;
 import acm.program.GraphicsProgram;
 import acm.graphics.GLine;
 
+/**
+   This class creates and moves the turtles;
+   It also checks if the turtles have passed the finish line.
+*/
 public class Turtles_Trivedi extends GraphicsProgram
 {
    public void run()
@@ -15,14 +23,58 @@ public class Turtles_Trivedi extends GraphicsProgram
       add(turt3);
       add(finish);
       pause(100);
+      String winner = "";
       
-      while(turt1.getX() < 600 && turt2.getX() < 600 && turt3.getX() < 600)
+      while(turt1.getX() < 600 || turt2.getX() < 600 || turt3.getX() < 600)
       {
-         turt1.forward(Math.random()*100);
-         turt2.forward(Math.random()*100);
-         turt3.forward(Math.random()*100);
+         if (turt1.getX() < 600)   
+            turt1.forward(Math.random()*100);
+         if (turt1.getX() > 600 && !(winner.equals("turt2")|| winner.equals("turt3")))
+         {
+            winner = "turt1";
+         }
+         
+         if (turt2.getX() < 600)
+            turt2.forward(Math.random()*100);
+         if (turt2.getX() > 600 && !(winner.equals("turt1")|| winner.equals("turt3")))
+         {
+            winner = "turt2";
+         }
+         
+         if (turt3.getX() < 600)
+            turt3.forward(Math.random()*100);
+         if (turt2.getX() > 600 && !(winner.equals("turt1") || winner.equals("turt2")))
+         {
+            winner = "turt3";
+         }
          pause(Math.random()*1000);
+         //pause(1);
       }
+      if (winner.equals("turt1"))
+      {
+         turt1.right(180);
+         turt1.forward(200);
+         turt1.left(90);
+         turt1.forward(200);
+      }
+      if (winner.equals("turt2"))
+      {
+         turt2.right(180);
+         turt2.forward(200);
+         turt2.left(90);
+         turt2.forward(200);
+      }
+      if (winner.equals("turt3"))
+      {
+         turt3.right(180);
+         turt3.forward(200);
+         turt3.left(90);
+         turt3.forward(200);
+      }
+
+   
+      System.out.println(winner);
+      
    }
    
    public static void main(String[] args)
